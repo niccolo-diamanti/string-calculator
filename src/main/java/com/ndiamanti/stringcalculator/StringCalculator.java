4e0f1d38657040c8e98be926a5eb7ed8cef6a4e8
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-18 22:44:39 +0100

    negative numbers exception

--- src/main/java/com/ndiamanti/stringcalculator/StringCalculator.java
+++ src/main/java/com/ndiamanti/stringcalculator/StringCalculator.java
@@ -1,13 +1,31 @@
 package com.ndiamanti.stringcalculator;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    public Integer add(String str) {
-        if (str.isEmpty())
+    public Integer add(String numbers) {
+        if (numbers.isEmpty())
             return 0;
-        else
-            return Arrays.stream(str.replaceAll("\n", ",").split(",")).map(Integer::valueOf).reduce(0, Integer::sum);
+        else {
+            Map<Boolean, List<Integer>> partition = Arrays.stream(numbers.replaceAll("\n", ",").split(","))
+                    .map(Integer::valueOf)
+                    .collect(Collectors.partitioningBy(i -> i < 0));
+            if (!partition.get(true).isEmpty())
+                throw new NegativeNumbersException(partition.get(true).stream()
+                        .map(String::valueOf)
+                        .collect(Collectors.joining(",")));
+            else
+                return partition.get(false).stream().reduce(0, Integer::sum);
+        }
+    }
+
+    public static class NegativeNumbersException extends RuntimeException {
+        public NegativeNumbersException(String numbers) {
+            super("Negatives not allowed: " + numbers);
+        }
     }
 }
