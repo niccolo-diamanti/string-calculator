e5245aa05298dba361d2236cc16a069dcc896eab
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-20 11:19:44 +0100

    refactor StringCalculator

--- src/main/java/com/ndiamanti/stringcalculator/StringCalculator.java
+++ src/main/java/com/ndiamanti/stringcalculator/StringCalculator.java
@@ -7,20 +7,34 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 
+    private final String defaultDelimiter = ",";
+
     public Integer add(String numbers) {
         if (numbers.isEmpty())
             return 0;
         else {
-            Map<Boolean, List<Integer>> partition = Arrays.stream(numbers.replaceAll("\n", ",").split(","))
-                    .map(Integer::valueOf)
-                    .collect(Collectors.partitioningBy(i -> i < 0));
+            Map<Boolean, List<Integer>> partition = splitNegativeAndPositiveNumbers(numbers);
             if (!partition.get(true).isEmpty())
-                throw new NegativeNumbersException(partition.get(true).stream()
-                        .map(String::valueOf)
-                        .collect(Collectors.joining(",")));
+                throw new NegativeNumbersException(joinNumbersByDelimiter(partition.get(true)));
             else
-                return partition.get(false).stream().reduce(0, Integer::sum);
+                return sumNumbers(partition.get(false));
         }
+    }
+
+    private Integer sumNumbers(List<Integer> numbers) {
+        return numbers.stream().reduce(0, Integer::sum);
+    }
+
+    private String joinNumbersByDelimiter(List<Integer> numbers) {
+        return numbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(defaultDelimiter));
+    }
+
+    private Map<Boolean, List<Integer>> splitNegativeAndPositiveNumbers(String numbers) {
+        return Arrays.stream(numbers.replaceAll("\n", defaultDelimiter).split(defaultDelimiter))
+                        .map(Integer::valueOf)
+                        .collect(Collectors.partitioningBy(i -> i < 0));
     }
 
     public static class NegativeNumbersException extends RuntimeException {
