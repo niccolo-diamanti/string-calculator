4ad320f2d6b0acc85a8661ba0f34c38dc0d88790
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-21 12:38:32 +0100

    StringCalculator handle multiple char delimiter, regex "safe"

--- src/main/java/com/ndiamanti/stringcalculator/StringCalculator.java
+++ src/main/java/com/ndiamanti/stringcalculator/StringCalculator.java
@@ -5,6 +5,7 @@ public interface StringCalculator {
     Integer MAX_NUMBER_ALLOWED = 1000;
     String DELIMITER_REGEX = "^//(.*)\\n";
     String DYNAMIC_DELIMITER_PREFIX = "//";
+    String DEFAULT_DELIMITER = ",";
 
     Integer add(String numbers);
 }
